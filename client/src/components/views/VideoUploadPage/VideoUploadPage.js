63f755a5dc43276471ef45120623204b6358072d
SSG0410 <ssuk0410@gmail.com>
2022-04-26 16:46:18 +0900

    220426

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone'
+import Axios from 'axios';
+
 const {TextArea} = Input;
 const {Title} = Typography;
-
+const PrivateOptions = [
+    {value: 0, label: "Private"},
+    {value: 1, label: "Public"}
+]
+const CategoryOptions = [
+    {value: 0, label: "Film & Animation"},
+    {value: 1, label: "Autos & Vehicles"},
+    {value: 2, label: "Option3"},
+    {value: 3, label: "Option4"}
+]
 function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Firm & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value)
+    }
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value)
+    }
+    const onDrop = (files) => {
+
+        let formData = new FormData;
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+        
+        Axios.post('/api/video/uploadfiles', formData, config).then(response => {
+            if(response.data.success){
+                console.log(response.data)
+
+                let variable = {
+                    url:response.data.url,
+                    fileName: response.data.fileName
+                }
+
+                setFilePath(response.data.url)
+
+                Axios.post('/api/video/thumbnail', variable).then(response => {
+                    if(response.data.success) {
+                        console.log(response.data)
+                        setDuration(response.data.fileDuration)
+                        setThumbnailPath(response.data.url)
+                    } else{
+                        alert('썸네일 제작 실패')
+                    }
+                })
+            } else{
+                alert('비디오 업로드 실패')
+            }
+        })
+    }
+
     return (
       <div style={{ maxWidth:'700px',margin:'2rem auto'}}>
           <div style={{textAlign:'center', marginBottom:'2rem'}}>
@@ -15,9 +81,9 @@ function VideoUploadPage() {
               <div style={{display:'flex', justifyContent:'space-between'}}>
                   {/*Drop zone*/}
                   <Dropzone
-                  onDrop
-                  multiple
-                  maxsize 
+                  onDrop={onDrop}
+                  multiple={false}
+                  maxsize={1000000000000}
                   >
                   {({getRootProps, getInputProps}) => (
                       <div style={{width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
@@ -28,35 +94,46 @@ function VideoUploadPage() {
                   )}
                   </Dropzone>
                   {/*Thumbnail*/}
+
+                  {ThumbnailPath &&
                   <div>
-                      <img src alt />
+                      <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail" />
                   </div>
+                  }
               </div>
             <br />
             <br />
             <label>Title</label>
             <Input
-                onChange
-                value
+                onChange={onTitleChange}
+                value={VideoTitle}
             />
             <br />
             <br />
             <label>Description</label>
             <TextArea
-                onChange
-                value
+                onChange={onDescriptionChange}
+                value={Description}
             />
             <br />
             <br />
 
-            <select onChange>
-                <option key value></option>
+            <select onChange={onPrivateChange}>
+
+                {PrivateOptions.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label} </option>
+                ))}
+
             </select>
             <br />
             <br />
 
-            <select onChange>
-                <option key value></option>
+            <select onChange={onCategoryChange}>
+
+                {CategoryOptions.map((item, index) => (
+                    <option key={index} value={item.value}>{item.label} </option>
+                ))}
+
             </select>
             <br />
             <br />
